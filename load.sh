1e2e60d539b623286650aa70f0a10e46d7f26e95
saiYu <yujinqiuustb@gmail.com>
2014-07-18 23:21:34 +0800

    add restart

--- load.sh
+++ load.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 function start() {
-    hugo server --theme=herring-cove  --verboseLog=true  --logFile=TimeCapsule/log/timecapsule.log  --baseUrl=http://chenmeilin.com -p 80 -s TimeCapsule
+    nohup hugo server --theme=herring-cove  --verboseLog=true  --logFile=TimeCapsule/log/timecapsule.log  --baseUrl=http://chenmeilin.com -w -p 80 -s TimeCapsule &
 }
 
 function stop() {
+	killall -9 hugo
+}
 
+function restart() {
+	stop && sleep 1
+	start
 }
 
 case C"$1" in
